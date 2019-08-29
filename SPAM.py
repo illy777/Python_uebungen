1cb7a8c0092ac53be731d9e1f787532aa745cbee
Isaac Yuki <isaacyuki@hotmail.com>
2019-08-30 00:56:35 +0200

    Geändert

--- SPAM.py
+++ SPAM.py
@@ -1,15 +1,16 @@
 from random import shuffle #mods verstehen
-with open("D:\isaac\Programmierung\Python Übungen\Superlatives.txt") as infile:
+with open("D:\isaac\Programmierung\Python Übungen\Python_uebungen\Superlatives.txt") as infile:
     txt = [line.strip() for line in infile] #das muss ich noch verstehen "with as" befehl und "for in"
 shuffle(txt)
-el2 = txt.pop()
-el1 = txt.pop()
+
 for strophe in range(4):
     for n in range(2):   
         for i in range(4):
             print("Spam ",end='')
         print()
     shuffle(txt)    
+    el1 = txt.pop()
+    el2 = txt.pop()
     print(("{} SPAM, {} SPAM".format(el1, el2)).upper()) #"{}" sind Platzhalter
     print()
  
---
+++ pop.py
@@ -0,0 +1,10 @@
+liste = "1 2 3 4 5".split()
+
+
+for ausgabe in range(2):
+    a1 = liste.pop() 
+    a2 = liste.pop() 
+    print("Zahlen: {} ; {}".format(a1, a2))
+    
+
+
